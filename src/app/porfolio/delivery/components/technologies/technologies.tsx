a682b5b8b6aeb457ab0cc490e3ff83e1d2f38883
Manumartin95 <manuel-harley@hotmail.com>
2023-07-05 18:56:50 +0200

    add prettier to eslint configuration

--- src/app/porfolio/delivery/components/technologies/technologies.tsx
+++ src/app/porfolio/delivery/components/technologies/technologies.tsx
@@ -1,71 +1,72 @@
 import styles from './technologies.module.css'
-import Image from "next/image";
+import Image from 'next/image'
 
-const TECHNOLOGIES: { name: string, image: string }[] = [{
+const TECHNOLOGIES: { name: string; image: string }[] = [
+  {
     name: 'React',
     image: '/icons/react-js-icon.svg'
-}, {
+  },
+  {
     name: 'Angular',
     image: '/icons/angular-icon.svg'
-}, {
+  },
+  {
     name: 'NextJS',
     image: '/icons/nextjs-icon.svg'
-}, {
+  },
+  {
     name: 'NestJS',
     image: '/icons/nest-js-icon.svg'
-},
-    {
-        name: 'HTML',
-        image: '/icons/html-icon.svg'
-    },
-    {
-        name: 'CSS',
-        image: '/icons/css-icon.svg'
-    },
-    {
-        name: 'JavaScript',
-        image: '/icons/javascript-icon.svg'
-    },
-    {
-        name: 'TypeScript',
-        image: '/icons/typescript-icon.svg'
-    },
-    {
-        name: 'Jest',
-        image: '/icons/jest-icon.svg'
-    },
-    {
-        name: 'Cypress',
-        image: '/icons/cypress.svg'
-    },
-    {
-        name: 'Sass',
-        image: '/icons/sass-icon.svg'
-    },
-    {
-        name: 'Docker',
-        image: '/icons/docker-icon.svg'
-    },
-    {
-        name: 'NodeJS',
-        image: '/icons/node-js-icon.svg'
-    }
-
-
+  },
+  {
+    name: 'HTML',
+    image: '/icons/html-icon.svg'
+  },
+  {
+    name: 'CSS',
+    image: '/icons/css-icon.svg'
+  },
+  {
+    name: 'JavaScript',
+    image: '/icons/javascript-icon.svg'
+  },
+  {
+    name: 'TypeScript',
+    image: '/icons/typescript-icon.svg'
+  },
+  {
+    name: 'Jest',
+    image: '/icons/jest-icon.svg'
+  },
+  {
+    name: 'Cypress',
+    image: '/icons/cypress.svg'
+  },
+  {
+    name: 'Sass',
+    image: '/icons/sass-icon.svg'
+  },
+  {
+    name: 'Docker',
+    image: '/icons/docker-icon.svg'
+  },
+  {
+    name: 'NodeJS',
+    image: '/icons/node-js-icon.svg'
+  }
 ]
 export const Technologies = () => {
-    return <>
-
-        <h2>Technologies i have used</h2>
-        <ul className={styles.list}>
-            {TECHNOLOGIES.map((item, index) => <li className={styles.item} key={index}><Image
-                src={item.image}
-                alt={item.name}
-                width={100}
-                height={100}
-                priority
-            /><span>{item.name}</span></li>)}
-        </ul>
+  return (
+    <>
+      <h2>Technologies i have used</h2>
+      <ul className={styles.list}>
+        {TECHNOLOGIES.map((item, index) => (
+          <li className={styles.item} key={index}>
+            <Image src={item.image} alt={item.name} width={100} height={100} priority />
+            <span>{item.name}</span>
+          </li>
+        ))}
+      </ul>
     </>
-
+  )
 }
