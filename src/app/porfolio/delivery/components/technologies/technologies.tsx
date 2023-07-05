dc98020eea6f6aaa0fda1b5602a712b15515e884
Manumartin95 <manuel-harley@hotmail.com>
2023-07-05 18:48:29 +0200

    add icons

--- src/app/porfolio/delivery/components/technologies/technologies.tsx
+++ src/app/porfolio/delivery/components/technologies/technologies.tsx
@@ -13,10 +13,49 @@ const TECHNOLOGIES: { name: string, image: string }[] = [{
 }, {
     name: 'NestJS',
     image: '/icons/nest-js-icon.svg'
-}]
+},
+    {
+        name: 'HTML',
+        image: '/icons/html-icon.svg'
+    },
+    {
+        name: 'CSS',
+        image: '/icons/css-icon.svg'
+    },
+    {
+        name: 'JavaScript',
+        image: '/icons/javascript-icon.svg'
+    },
+    {
+        name: 'TypeScript',
+        image: '/icons/typescript-icon.svg'
+    },
+    {
+        name: 'Jest',
+        image: '/icons/jest-icon.svg'
+    },
+    {
+        name: 'Cypress',
+        image: '/icons/cypress.svg'
+    },
+    {
+        name: 'Sass',
+        image: '/icons/sass-icon.svg'
+    },
+    {
+        name: 'Docker',
+        image: '/icons/docker-icon.svg'
+    },
+    {
+        name: 'NodeJS',
+        image: '/icons/node-js-icon.svg'
+    }
+
+
+]
 export const Technologies = () => {
     return <>
-        
+
         <h2>Technologies i have used</h2>
         <ul className={styles.list}>
             {TECHNOLOGIES.map((item, index) => <li className={styles.item} key={index}><Image
