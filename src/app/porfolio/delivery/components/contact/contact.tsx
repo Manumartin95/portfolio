d3699f0f5d333d756ae26eb54a709912eb80246e
Manumartin95 <manuel-harley@hotmail.com>
2023-07-05 18:49:23 +0200

    update styling

--- src/app/porfolio/delivery/components/contact/contact.tsx
+++ src/app/porfolio/delivery/components/contact/contact.tsx
@@ -1,5 +1,13 @@
+'use client'
+import {Button} from "@/app/shared/delivery/components/button/button";
+import styles from './contact.module.css'
+
 export const Contact = () => {
-    return <div>
+    return <div className={styles.wrapper}>
         <a href="mailto:email@example.com">Contact me</a>
+        <Button type='button' onClick={() => {
+            console.log('Hello world')
+        }
+        }>Click me!</Button>
     </div>
 }
--- src/app/shared/delivery/components/button/button.tsx
+++ src/app/shared/delivery/components/button/button.tsx
@@ -1,12 +1,13 @@
 import {FC, PropsWithChildren} from "react";
+import styles from './button.module.css'
 
 interface ButtonProps {
-    class: string;
+    class?: string;
     type: 'submit' | 'button' | 'reset';
     onClick: () => void;
-    disabled: boolean;
+    disabled?: boolean;
 }
 
 export const Button: FC<PropsWithChildren<ButtonProps>> = (props) => {
-    return <button className={''} onClick={props.onClick} type={props.type}>{props.children}</button>
+    return <button className={styles.button} onClick={() => props.onClick()} type={props.type}>{props.children}</button>
 }
