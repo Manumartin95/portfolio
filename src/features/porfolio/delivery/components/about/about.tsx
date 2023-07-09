f348c03fa725c62a747c76e6c5150e168d4f3978
Manumartin95 <manuel-harley@hotmail.com>
2023-07-09 23:09:14 +0200

    add i18n test

--- src/features/porfolio/delivery/components/about/about.tsx
+++ src/features/porfolio/delivery/components/about/about.tsx
@@ -1,20 +1,29 @@
-import styles from "./about.module.css";
-import Image from "next/image";
+import styles from './about.module.css'
+import Image from 'next/image'
+import { useTranslation } from 'next-i18next'
 
 export const About = () => {
-    return <>
-        <div className={styles.wrapper}>
-            <Image
-                src="/images/profile.jpeg"
-                alt="Profile photo"
-                width={350}
-                height={350}
-                className={styles.image}
-                priority
-            />
-            <h3>Full name</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae ea et facere, fuga iste laudantium
-                officia ut veniam. Blanditiis commodi eos error, expedita fugit id magnam natus quis soluta vero?</p>
-        </div>
+  const { t } = useTranslation('common')
+
+  return (
+    <>
+      <div className={styles.wrapper}>
+        {t('test')}
+        <Image
+          src="/images/profile.jpeg"
+          alt="Profile photo"
+          width={350}
+          height={350}
+          className={styles.image}
+          priority
+        />
+        <h3>Full name</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae ea et facere, fuga iste
+          laudantium officia ut veniam. Blanditiis commodi eos error, expedita fugit id magnam natus
+          quis soluta vero?
+        </p>
+      </div>
     </>
+  )
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,18 @@
 import Home from '@/features/porfolio/delivery/home/home'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { GetStaticProps } from 'next'
+import { SSRConfig } from 'next-i18next'
 
 const Index = () => {
   return <Home></Home>
+}
+
+export const getStaticProps: GetStaticProps<SSRConfig> = async (context) => {
+  return {
+    props: {
+      ...(await serverSideTranslations(context.locale!, ['common', 'footer']))
+    }
+  }
 }
 
 export default Index
