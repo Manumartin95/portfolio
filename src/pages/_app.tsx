06d63921cc5e33b4a19eac37fc66ecbe88e514e7
Manumartin95 <manuel-harley@hotmail.com>
2023-07-15 13:19:23 +0100

    add spacing custom variables

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import { AppProps } from 'next/app'
 import { appWithTranslation } from 'next-i18next'
 import { Layout } from '@/features/shared/delivery/components/layout/layout'
+import '../styles/reset.css'
+import '../styles/theme.css'
+import '../styles/globals.css'
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
